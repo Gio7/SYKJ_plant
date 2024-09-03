de7f625e2964aacdcef6e48ddc6538a1fcf093bd
gio <8524020@qq.com>
2024-09-03 19:47:17 +0800

    删除测试用的文件

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -69,16 +69,4 @@ class DefaultFirebaseOptions {
     iosClientId: 'com.plant.identifier.diagnosis.scanner.app',
     iosBundleId: 'com.plant.identifier.diagnosis.scanner.app',
   );
-
-  static const FirebaseOptions iosTest = FirebaseOptions(
-    apiKey: 'AIzaSyD6NsCMq7-HL8ytu1gfK4sOdOSC0w0N1WE',
-    appId: '1:361651658601:ios:ae5f05d1c815d4e0d0e7f8',
-    messagingSenderId: '361651658601',
-    projectId: 'test-bundle-2024',
-    databaseURL: 'https://flutterfire-e2e-tests-default-rtdb.europe-west1.firebasedatabase.app',
-    storageBucket: 'test-bundle-2024.appspot.com',
-    androidClientId: 'com.plant.identification',
-    iosClientId: 'com.plant.identification',
-    iosBundleId: 'com.plant.identification',
-  );
 }
