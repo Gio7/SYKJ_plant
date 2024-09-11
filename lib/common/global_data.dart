47e9234b689305e72e4177997382745e7d46d7fa
gio <8524020@qq.com>
2024-09-11 15:26:02 +0800

    build iOS不区分release接口环境

--- lib/common/global_data.dart
+++ lib/common/global_data.dart
@@ -1,11 +1,15 @@
 // import 'buy_shop.dart';
 
 import 'package:flutter/foundation.dart';
+import 'package:get/get.dart';
 
 import 'buy_shop.dart';
 
 class GlobalData {
   static String get baseUrl {
+    if (GetPlatform.isIOS) {
+      return 'http://192.168.0.111:19300';
+    }
     if (kDebugMode) {
       //https://test.plantidentifier.co
       return 'http://192.168.0.111:19300';
