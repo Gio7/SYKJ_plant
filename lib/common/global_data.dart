74c63993f20707f69bc5cca028ac5a7d0bb555ec
gio <8524020@qq.com>
2024-11-29 11:49:28 +0800

    Revert "ios 生产环境"
    This reverts commit 87f22ae6e1edad7f3cf35236ca21d717f855e388.

--- lib/common/global_data.dart
+++ lib/common/global_data.dart
@@ -7,7 +7,7 @@ class GlobalData {
     if (defaultTargetPlatform == TargetPlatform.android) {
       return kDebugMode ? 'http://192.168.0.111:19300' : 'https://api.plantidentifier.co';
     } else if (packageName.contains('identification')){
-      return 'https://api.plantidentifier.co';
+      return 'http://192.168.0.111:19300';
     } else {
       return 'https://api.plantidentifier.co';
     }
